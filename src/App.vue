32f9f941820769fccd440e5b0fb22928e30f2330
pan987 <1912412079@qq.com>
2019-10-25 17:49:45 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,9 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div id="app">hm-toutiao-pc</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script>export default {
+  name: 'app'
+}; </script>
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="less"></style>
